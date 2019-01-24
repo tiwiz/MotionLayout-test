4812269796ffadadf900ca8a812b4b0e6d88a7fe
Roberto Orgiu <roberto.orgiu@gmail.com>
2019-01-24 17:19:29 +0100

    Create project

---
+++ app/src/main/java/net/orgiu/tickets/MainActivity.kt
@@ -0,0 +1,39 @@
+package net.orgiu.tickets
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import net.orgiu.tickets.utils.EndAnimation
+
+class MainActivity : AppCompatActivity() {
+
+    private val endAnimation = EndAnimation()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        validation_container.setTransitionListener(endAnimation)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.play -> toggleAnimation()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun toggleAnimation() {
+        if (endAnimation.isStart()) {
+            validation_container.transitionToEnd()
+        } else {
+            validation_container.transitionToStart()
+        }
+    }
+}
---
+++ app/src/main/java/net/orgiu/tickets/utils/EndAnimation.kt
@@ -0,0 +1,20 @@
+package net.orgiu.tickets.utils
+
+import android.support.constraint.motion.MotionLayout
+
+class EndAnimation : MotionLayout.TransitionListener {
+
+    private var isStart = true
+
+    override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) = Unit
+
+    override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
+
+    override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) = Unit
+
+    override fun onTransitionCompleted(target: MotionLayout?, p1: Int) {
+        isStart = isStart.not()
+    }
+
+    fun isStart() = isStart
+}
